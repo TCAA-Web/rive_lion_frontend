39232e2798e9af0591d2dfa39c61696abc05aa8e
kasperfryd <kfry@techcollege.dk>
2024-12-10 15:30:52 +0100

    updated URL

--- scripts/socketController.js
+++ scripts/socketController.js
@@ -1,9 +1,7 @@
 import { io } from "https://cdn.socket.io/4.3.2/socket.io.esm.min.js";
 
 // Initializes the socket IO connection
-export const socket = io(
-  "https://rivelionbackend-production.up.railway.app:3000"
-);
+export const socket = io("https://rivelionbackend-production.up.railway.app");
 export const ioSocket = socket.connect(
-  "https://rivelionbackend-production.up.railway.app:3000"
+  "https://rivelionbackend-production.up.railway.app"
 );
