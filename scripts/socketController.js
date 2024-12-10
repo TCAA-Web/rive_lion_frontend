436542c39a2db5d59d1f21006020e7fd79c0d2d3
kasperfryd <kfry@techcollege.dk>
2024-12-10 14:21:12 +0100

    updated path

--- scripts/socketController.js
+++ scripts/socketController.js
@@ -1,5 +1,7 @@
 import { io } from "https://cdn.socket.io/4.3.2/socket.io.esm.min.js";
 
 // Initializes the socket IO connection
-export const socket = io("localhost:3000");
-export const ioSocket = socket.connect("localhost:3000");
+export const socket = io("https://stellar-muffin-8bae97.netlify.app");
+export const ioSocket = socket.connect(
+  "https://stellar-muffin-8bae97.netlify.app"
+);
