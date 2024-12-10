9ff968630c639f1c2f49067d34ea0bdab8bec9a6
kasperfryd <kfry@techcollege.dk>
2024-12-10 14:45:40 +0100

    updated url

--- scripts/socketController.js
+++ scripts/socketController.js
@@ -1,5 +1,7 @@
 import { io } from "https://cdn.socket.io/4.3.2/socket.io.esm.min.js";
 
 // Initializes the socket IO connection
-export const socket = io("https://rivebackendlion.netlify.app");
-export const ioSocket = socket.connect("https://rivebackendlion.netlify.app");
+export const socket = io("https://rivebackendlion.netlify.app/api/");
+export const ioSocket = socket.connect(
+  "https://rivebackendlion.netlify.app/api/"
+);
