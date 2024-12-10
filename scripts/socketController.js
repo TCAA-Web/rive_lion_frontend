10b8883c1c12d40c66bc8d55c980f4703a5c3ce6
kasperfryd <kfry@techcollege.dk>
2024-12-10 15:04:40 +0100

    update

--- scripts/socketController.js
+++ scripts/socketController.js
@@ -1,7 +1,9 @@
 import { io } from "https://cdn.socket.io/4.3.2/socket.io.esm.min.js";
 
 // Initializes the socket IO connection
-export const socket = io("rivelionbackend-production.up.railway.app:3000");
+export const socket = io(
+  "https://rivelionbackend-production.up.railway.app:3000"
+);
 export const ioSocket = socket.connect(
-  "rivelionbackend-production.up.railway.app:3000"
+  "https://rivelionbackend-production.up.railway.app/:3000"
 );
